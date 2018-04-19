88f654d357ad3c47b28d1829c217dde26bb7aff8
ChangJoo Park <pcjpcj2@gmail.com>
2018-04-20 00:37:48 +0900

    Article repository를 이용해 데이터 저장

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,14 @@ createConnection().then(async connection => {
     const newArticle = new Article();
     newArticle.body = 'My First Article Body'
     newArticle.title = 'My First Article Title'
+
     try {
-        const savedArticle = await connection.manager.save(newArticle);
-        console.log(savedArticle)
+        await connection.getRepository(Article).save(newArticle)
     } catch (error) {
         console.log(error)
     }
+
     const articles = await connection.manager.find(Article);
     console.log(articles)
-
-    // console.log("Here you can setup and run express/koa/any other framework.");
 
 }).catch(error => console.log(error));
